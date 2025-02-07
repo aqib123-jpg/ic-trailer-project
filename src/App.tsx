18ef06b5ee6f29e9d818516dbc83ff6d81f9d028
aqib123-jpg <saaqibrehhman@gmail.com>
2025-02-07 21:13:44 +0500

    yess

--- src/App.tsx
+++ src/App.tsx
@@ -107,7 +107,7 @@ const App: React.FC = () => {
             <LogicGateComponent key={gate.id} gate={gate} />
           ))}
         </div>
-        {/* comment here twenty feburary */}
+        {/* comment here twent feburary */}
         <div className="flex-1">
           <DropZone onDrop={handleDrop} />
         </div>
